0791f80cadd4084d0f7b725e7de30186d226d240
Simon Walker <107306830+simonnjwalker@users.noreply.github.com>
2022-07-21 13:10:00 +1000

    Created project.cs
    This is a Model that sits in the Project area in a fresh MVC application
    note: you need to run dotnet-aspnet-codegenerator area Project to create areas

---
+++ project.cs
@@ -0,0 +1,8 @@
+namespace xx.Areas.Project.Models;
+// note: you need to run dotnet-aspnet-codegenerator area Project to create areas
+public class project
+{
+        public Guid Id = new Guid();
+        public string Name = "";
+        public string Desc = "";
+}
