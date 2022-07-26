1725da2b251c083aaaa6056ed352cffbecae93cb
Simon Walker <107306830+simonnjwalker@users.noreply.github.com>
2022-07-27 09:54:44 +1000

    Update and rename project.cs to Project.cs

--- Project.cs
+++ Project.cs
@@ -1,6 +1,6 @@
 namespace xx.Areas.Project.Models;
 // note: you need to run dotnet-aspnet-codegenerator area Project to create areas
-public class project
+public class Project
 {
         public Guid Id = new Guid();
         public string Name = "";
