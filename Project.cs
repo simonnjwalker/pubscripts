adc2cf7ee42b86b3c9282f9fa3a69a5dbe8936cb
Simon Walker <107306830+simonnjwalker@users.noreply.github.com>
2022-07-27 12:44:07 +1000

    Update Project.cs

--- Project.cs
+++ Project.cs
@@ -4,6 +4,6 @@ public class Project
 {
         [Key]
         public Guid Id {get;set;};
-        public string Name {get;set;}
+        public string Name {get;set;};
         public string Desc {get;set;};
 }
