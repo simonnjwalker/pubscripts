dd7869bea086e87e8ee3e2576c312b7dcf5485d9
Simon Walker <107306830+simonnjwalker@users.noreply.github.com>
2022-07-27 12:43:52 +1000

    Update Project.cs

--- Project.cs
+++ Project.cs
@@ -2,7 +2,8 @@ namespace xx.Areas.Project.Models;
 // note: you need to run dotnet-aspnet-codegenerator area Project to create areas
 public class Project
 {
-        public Guid Id = new Guid();
-        public string Name = "";
-        public string Desc = "";
+        [Key]
+        public Guid Id {get;set;};
+        public string Name {get;set;}
+        public string Desc {get;set;};
 }
