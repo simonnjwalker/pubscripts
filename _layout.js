d3426dbc48d72521433501ca60473d596144c74d
Simon Walker <107306830+simonnjwalker@users.noreply.github.com>
2022-09-26 12:07:33 +1000

    fixed more null references

--- _layout.js
+++ _layout.js
@@ -9,17 +9,19 @@
     }
     window.onscroll = function() {
         var header_navbar = document.querySelector(".hero-section-wrapper-5 .header");
-        var sticky = header_navbar.offsetTop;
-        if (window.pageYOffset > sticky) {
-            header_navbar.classList.add("sticky");
-        } else {
-            header_navbar.classList.remove("sticky");
-        }
-        var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
+        if(header_navbar != null){
+                var sticky = header_navbar.offsetTop;
+            if (window.pageYOffset > sticky) {
+                header_navbar.classList.add("sticky");
+            } else {
+                header_navbar.classList.remove("sticky");
+            }
+            var backToTo = document.querySelector(".scroll-top");
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                backToTo.style.display = "flex";
+            } else {
+                backToTo.style.display = "none";
+            }
         }
     };
     let navbarToggler6 = document.querySelector(".header-6 .navbar-toggler");
@@ -36,17 +38,23 @@
 
     function onScroll(event) {
         var sections = document.querySelectorAll('.page-scroll');
-        var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-        for (var i = 0; i < sections.length; i++) {
-            var currLink = sections[i];
-            var val = currLink.getAttribute('href');
-            var refElement = document.querySelector(val);
-            var scrollTopMinus = scrollPos + 73;
-            if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
-                document.querySelector('.page-scroll').classList.remove('active');
-                currLink.classList.add('active');
-            } else {
-                currLink.classList.remove('active');
+        if(sections != null){
+            var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+            for (var i = 0; i < sections.length; i++) {
+                var currLink = sections[i];
+                var val = currLink.getAttribute('href');
+                if(val != null){        
+                    var refElement = document.querySelector(val);
+                    if(refElement != null){
+                        var scrollTopMinus = scrollPos + 73;
+                        if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+                            document.querySelector('.page-scroll').classList.remove('active');
+                            currLink.classList.add('active');
+                        } else {
+                            currLink.classList.remove('active');
+                        }
+                    }
+                }
             }
         }
     };
