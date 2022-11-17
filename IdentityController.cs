91d21ab6eccabbf5cf4e2bccec2ab885dcfac8e7
Simon Walker <107306830+simonnjwalker@users.noreply.github.com>
2022-11-17 15:37:16 +1000

    Update IdentityController.cs

--- IdentityController.cs
+++ IdentityController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using xp.Controllers;
+using xx.Controllers;
 #pragma warning disable CS8618
 namespace Seamlex.MyEdApps
 {
